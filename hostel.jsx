8ca3301d1b04bd306b6a28362b0f53860c6cfb9d
Saket Anand <saketanand200911@gmail.com>
2026-09-24 11:32:16 +0530

    Fix local Google OAuth redirect

--- hostel.jsx
+++ hostel.jsx
@@ -359,7 +359,9 @@ async function handleAuthSubmit(e) {
 
 // Google OAuth Handler
 function triggerGoogleAuth() {
-    const returnTo = window.location.href.split('?')[0];
+    const returnTo = ['localhost', '127.0.0.1'].includes(window.location.hostname)
+        ? 'http://localhost:5500/hostel.html'
+        : window.location.href.split('?')[0];
     window.location.assign(`${API_BASE_URL}/auth/google/start?returnTo=${encodeURIComponent(returnTo)}`);
 }
 
