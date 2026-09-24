9c35df6be65f8179c6439d5adcc6fa6801b735b6
Saket Anand <saketanand200911@gmail.com>
2026-09-24 13:41:51 +0530

    Show all logged-in users in MongoDB playground

--- campushaven/backend/campushaven.mongodb.js
+++ campushaven/backend/campushaven.mongodb.js
@@ -1,11 +1,11 @@
 use('campushaven');
 
 db.users.find(
-    {},
+    { lastLoginAt: { $exists: true } },
     {
         passwordHash: 0
     }
-).sort({ createdAt: -1 }).limit(20);
+).sort({ lastLoginAt: -1, createdAt: -1 });
 
 db.authSessions.find(
     {},
